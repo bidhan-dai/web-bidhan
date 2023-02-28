5eaf8e96c82f7474591c889a27bb05bde81a1379
bidhan-dai <126590894+bidhan-dai@users.noreply.github.com>
2023-02-28 20:56:21 +0545

    Add files via upload

---
+++ ui-bidhan/script.js
@@ -0,0 +1,42 @@
+// ----------------js for hamburger menu-----------------
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+// ----------------js for hamburger menu-----------------
+
+// ---------------------------codes for sticky navbar-----------------------------------
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+
+  lastScroll = currentScroll;
+});
+
+// ---------------------------codes for sticky navbar-----------------------------------

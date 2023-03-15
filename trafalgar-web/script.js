55ba498aeef9607005eabbab635cdf1b20ea4cf4
Bidhan Tamang <126590894+bidhan-dai@users.noreply.github.com>
2023-03-15 10:17:25 +0545

    Add files via upload

---
+++ trafalgar-web/script.js
@@ -0,0 +1,48 @@
+//mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+//for sticky nav bar
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+
+// scroll to top
+
+const toTop=document.querySelector(".to-top");
+
+window.addEventListener("scroll",()=>{
+  if(window.pageYOffset>300){
+    toTop.classList.add("active");
+  }
+  else{
+  toTop.classList.remove("active");
+  }
+})
